961d89fa1f0d7e7e72230c115bee25d94f26dd9c
sea <artur.semenenko@nag.ch>
2022-12-16 10:30:26 +0100

    CanbanCard.java

--- src/main/java/org/example/CanbanCard.java
+++ src/main/java/org/example/CanbanCard.java
@@ -1,8 +1,26 @@
 package org.example;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class CanbanCard {
     private UUID uuid;
+    private int numberInOrder;
 
+    private CanbanCardStatus status;
+
+    private String title;
+    private String description;
+    private Date lastEditedDate;
+
+
+    private void addCardToRelease(CanbanRelease canbanRelease){
+
+    }
+}
+
+enum CanbanCardStatus{
+    TODO,
+    PROCESSING,
+    DONE
 }
