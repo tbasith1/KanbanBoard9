b5ad54b9c97684d3af83d304f07273b7c118cb6a
sea <artur.semenenko@nag.ch>
2022-12-16 10:03:47 +0100

    Skeleton

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nKanbanBoard9!\nBy Thomas and Artur\n\n");
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        System.out.println(command);
     }
+
 }
