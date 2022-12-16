127b8c27a31ca1169b3fa76fd330ad2a83cb9dfa
Sidler <sit@nag.ch>
2022-12-16 10:18:49 +0100

    update KanbanBoard

--- src/main/java/org/example/KanbanBoard.java
+++ src/main/java/org/example/KanbanBoard.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Data representation of all data needed in the KanbanBoard application
  */
 
 public class KanbanBoard {
-  List<Card> = new ArrayList<Card>();
-  List<Release> = new ArrayList<Release>();
+  List<Card> cardData = new ArrayList<Card>();
+  List<Release> releaseData = new ArrayList<Release>();
 }
